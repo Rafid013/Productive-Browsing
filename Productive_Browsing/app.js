2484164c701082c8cb0b05828f04474f8b864034
Rafid013 <1405013.ahmr@ugrad.cse.buet.ac.bd>
2017-11-07 22:16:50 +0600

    The work that was already done in the express framework

--- Productive_Browsing/app.js
+++ Productive_Browsing/app.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var extension_home = require('./routes/extension_home');
 
 var app = express();
 
@@ -22,8 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', extension_home);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
